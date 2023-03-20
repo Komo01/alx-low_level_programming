337863a1103d73abb81c6e27ef5d6e3d8e343549
Komo01 <corealignment@yahoo.com>
2023-03-20 06:54:15 -0700

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,8 +7,8 @@ int main(void)
 {
 	printf("size of char is=%i byte\n",sizeof(char));
 	printf("size of  int is=%i byte\n",sizeof(int));
-	printf("size of long int is=%i byte\n",sizeof(longint));
-	printf("size of long long int is=%i byte\n",sizeof(longlongint));
+	printf("size of long int is=%i byte\n",sizeof(long int));
+	printf("size of long long int is=%i byte\n",sizeof(long long int));
 	printf("size of float is=%i byte\n",sizeof(float));
 	return (0);
 }

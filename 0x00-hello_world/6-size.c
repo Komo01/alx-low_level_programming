6032d259357a116eaf13102db79221a58bffdb5f
Komo01 <corealignment@yahoo.com>
2023-03-20 06:26:07 -0700

    size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+/**
+ * main- Entry point
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	printf("size of char is=%i byte\n",sizeof(char));
+	printf("size of  int is=%i byte\n",sizeof(int));
+	printf("size of long int is=%i byte\n",sizeof(longint));
+	printf("size of long long int is=%i byte\n",sizeof(longlongint));
+	printf("size of float is=%i byte\n",sizeof(float));
+	return (0);
+}

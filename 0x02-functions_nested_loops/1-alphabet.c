d5192989e37582577f9cdbc2ed405d47c787f881
Komo01 <corealignment@yahoo.com>
2023-03-22 02:21:10 -0700

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print all alphabet in lowercase
+ */
+
+void print_alpgabet (void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		_putchar (letter);
+	_putchar ('\n');
+}
